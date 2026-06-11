073a52d89563eabaab427448e8cc8e3ee5e492e5
shashi <ds_talluri@yahoo.com>
2026-06-11 13:42:29 +0530

    added 2 private subnets

--- main.tf
+++ main.tf
@@ -18,50 +18,6 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
-resource "aws_subnet" "pub_01" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.0.0/24"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name        = "pub-01"
-    Environment = var.environment
-  }
-}
-
-resource "aws_subnet" "pub_02" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name        = "pub-02"
-    Environment = var.environment
-  }
-}
-
-resource "aws_subnet" "app_01" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = "${var.aws_region}a"
-
-  tags = {
-    Name        = "app-01"
-    Environment = var.environment
-  }
-}
-
-resource "aws_subnet" "app_02" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.4.0/24"
-  availability_zone = "${var.aws_region}b"
-
-  tags = {
-    Name        = "app-02"
-    Environment = var.environment
-  }
-}
-
 data "aws_ami" "amazon_linux" {
   most_recent = true
   owners      = ["amazon"]
